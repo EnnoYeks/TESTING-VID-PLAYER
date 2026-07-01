5a61e7e4f7a331d6f33bd566cc9123da0193e3e8
Enno Yeks <ennoyeks@gmail.com>
2026-07-01 10:54:58 +0300

    Initialize Supabase client in supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl="YOUR_URL";
+const supabaseKey="YOUR_KEY";
+
+export const supabase=createClient(
+    supabaseUrl,
+    supabaseKey
+);
