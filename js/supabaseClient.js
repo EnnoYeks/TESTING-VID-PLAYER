f231309d92ec7fe11b3fff0ab4b9ccace9dbbac0
Enno Yeks <ennoyeks@gmail.com>
2026-07-01 11:13:03 +0300

    Update Supabase URL and key in supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl="YOUR_URL";
-const supabaseKey="YOUR_KEY";
+const supabaseUrl="https://wtrtrjflpvetbksithrb.supabase.co/storage/v1/object/public/videos/trialvid.mp4";
+const supabaseKey="453a17efd985256b7755e6e26575228d";
 
 export const supabase=createClient(
     supabaseUrl,
